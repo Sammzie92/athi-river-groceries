6a8b8917102d5147a19adc779bf4dd302bebc4f1
Sammzie92 <122966796+Sammzie92@users.noreply.github.com>
2023-04-21 12:23:00 +0300

    files updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    backgroundSize: {
-      'auto': 'auto',
-      'cover': 'cover',
-      'contain': 'contain',
-      '50%': '50%',
-      '16': '4rem',
-    }
-  }
+    extend: {},
+  },
+  plugins: [],
 }
